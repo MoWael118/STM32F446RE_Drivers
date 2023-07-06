74ee7607accf05235893e61e72c12ada28294580
Mohamed Wael <mwael2915@gmail.com>
2023-07-06 15:29:13 +0300

    NVIC Program Version 1.0 Added!

--- MCAL/NVIC/Src/NVIC_Program.c
+++ MCAL/NVIC/Src/NVIC_Program.c
@@ -11,75 +11,117 @@
 #include "../Inc/NVIC_Private.h"
 
 /***********************************
- * @function 		:	NVIC_u8EnableInt
+ * @function 		:	NVIC_u8EnableIRQ
  * @brief			:	Enable the NVIC Interrupt for specific IRQ
  * @parameter[in]	:	IRQ_Num
  * @retval			:	Error State
  */
 
-uint8_t NVIC_u8EnableInt(uint8_t IRQ_Num)
+uint8_t NVIC_u8EnableIRQ(uint8_t IRQ_Num)
 {
 	uint8_t Error_State = OK;
 
+	if (IRQ_Num >=0 && IRQ_Num<97)
+	{
+		/*Set (Enable) The Corresponding IRQ Bit*/
+		NVIC->NVIC_ISER[IRQ_Num/32] = (1<<(IRQ_Num%32));
+	}
+	else
+	{
+		Error_State = WRONG_IRQ_NUM;
+	}
 	return Error_State;
 }
 
 /***********************************
- * @function 		:	NVIC_u8DisableInt
+ * @function 		:	NVIC_u8DisableIRQ
  * @brief			:	Disable the NVIC Interrupt for specific IRQ
  * @parameter[in]	:	IRQ_Num
  * @retval			:	Error State
  */
 
-uint8_t NVIC_u8DisableInt(uint8_t IRQ_Num)
+uint8_t NVIC_u8DisableIRQ(uint8_t IRQ_Num)
 {
 	uint8_t Error_State = OK;
-
-		return Error_State;
+	if (IRQ_Num >=0 && IRQ_Num<97)
+	{
+		/*Set (Disable) The Corresponding IRQ Bit*/
+		NVIC->NVIC_ICER[IRQ_Num/32] = (1<<(IRQ_Num%32));
+	}
+	else
+	{
+		Error_State = WRONG_IRQ_NUM;
+	}
+	return Error_State;
 }
 
 
 /***********************************
- * @function 		:	NVIC_u8SetPendFlag
- * @brief			:	Set the Interrupt Pending flag for specific IRQ
+ * @function 		:	NVIC_u8SetPendingIRQ
+ * @brief			:	Set the Interrupt State into Pending for specific IRQ
  * @parameter[in]	:	IRQ_Num
  * @retval			:	Error State
  */
 
-uint8_t NVIC_u8SetPendFlag(uint8_t IRQ_Num)
+uint8_t NVIC_u8SetPendingIRQ(uint8_t IRQ_Num)
 {
 	uint8_t Error_State = OK;
 
-		return Error_State;
+	if (IRQ_Num >=0 && IRQ_Num<97)
+	{
+		/*Set (Enable) The Corresponding IRQ Bit*/
+		NVIC->NVIC_ISPR[IRQ_Num/32] = (1<<(IRQ_Num%32));
+	}
+	else
+	{
+		Error_State = WRONG_IRQ_NUM;
+	}
+	return Error_State;
 }
 
 
 /***********************************
- * @function 		:	NVIC_u8ClearPendFlag
+ * @function 		:	NVIC_u8ClearPendingIRQ
  * @brief			:	Clear the Interrupt Pending flag for specific IRQ
  * @parameter[in]	:	IRQ_Num
  * @retval			:	Error State
  */
 
-uint8_t NVIC_u8ClearPendFlag(uint8_t IRQ_Num)
+uint8_t NVIC_u8ClearPendingIRQ(uint8_t IRQ_Num)
 {
 	uint8_t Error_State = OK;
-
-		return Error_State;
+	if (IRQ_Num >=0 && IRQ_Num<97)
+	{
+		/*Set (CLear Pending) The Corresponding IRQ Bit*/
+		NVIC->NVIC_ICPR[IRQ_Num/32] = (1<<(IRQ_Num%32));
+	}
+	else
+	{
+		Error_State = WRONG_IRQ_NUM;
+	}
+	return Error_State;
 }
 
 
 /***********************************
- * @function 		:	NVIC_u8ReadActiveFlag
+ * @function 		:	NVIC_u8GetActiveIRQ
  * @brief			:	Clear the Interrupt Pending flag for specific IRQ
  * @parameter[in]	:	IRQ_Num
  * @parameter[out]  :   Flag_Value (Active Flag Value)
  * @retval			:	Error State
  */
 
-uint8_t NVIC_u8ReadActiveFlag(uint8_t IRQ_Num,uint8_t * Flag_Value)
+uint8_t NVIC_u8GetActiveIRQ(uint8_t IRQ_Num,uint8_t * Flag_Value)
 {
 	uint8_t Error_State = OK;
-
-		return Error_State;
+	if (IRQ_Num >=0 && IRQ_Num<97)
+	{
+		/*Get The Corresponding IRQ Bit State*/
+		*Flag_Value = ((NVIC->NVIC_IABR[IRQ_Num/32]>>(IRQ_Num%32))&1);
+	}
+	else
+	{
+		Error_State = WRONG_IRQ_NUM;
+	}
+	return Error_State;
 }
