d6fcd71862a4d3a80a3a8e7acc77fd65720fec5e
Mohamed Wael <mwael2915@gmail.com>
2023-07-17 11:38:13 +0300

    Priority Errors Added!

--- LIBRARY/ErrTypes.h
+++ LIBRARY/ErrTypes.h
@@ -8,7 +8,9 @@
 #ifndef ERRTYPES_H_
 #define ERRTYPES_H_
 
-enum ErrorState {  NOK , OK ,Null_Pointer,InvalidPORTPIN,WRONG_IRQ_NUM};
+enum ErrorState {  NOK , OK ,Null_Pointer,InvalidPORTPIN,WRONG_IRQ_NUM
+				,WRONG_PRIORITY_GROUPING
+				,WRONG_PRIORITY_NUM};
 #define NULL ((void *)0)
 
 #endif /* ERRTYPES_H_ */
