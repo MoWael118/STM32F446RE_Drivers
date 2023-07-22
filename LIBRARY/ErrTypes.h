caf1c3df132057ff8163700fc467fde161887f30
Mohamed Wael <mwael2915@gmail.com>
2023-07-22 18:15:38 +0300

    EXTI Error types added!

--- LIBRARY/ErrTypes.h
+++ LIBRARY/ErrTypes.h
@@ -1,16 +1,20 @@
 /*
- * Defines.h
- *
- *  Created on: May 1, 2023
- *      Author: mhmd wael
+ *@file		:	ErrTypes.h
+ *@author	: 	Mohamed Wael
+ *@brief	:	Main Errors FOR STM32F446xx MCU
  */
 
 #ifndef ERRTYPES_H_
 #define ERRTYPES_H_
 
-enum ErrorState {  NOK , OK ,Null_Pointer,InvalidPORTPIN,WRONG_IRQ_NUM
+enum ErrorState {  NOK , OK ,Null_Pointer,InvalidPORTPIN
+				,WRONG_IRQ_NUM
 				,WRONG_PRIORITY_GROUPING
-				,WRONG_PRIORITY_NUM};
+				,WRONG_PRIORITY_NUM
+				,WRONG_EXTI_LINE
+				,WRONG_EXTI_STATE
+				,WRONG_EXTI_TRRIGERTYPE
+				,WRONG_EXTI_PORT};
 #define NULL ((void *)0)
 
 #endif /* ERRTYPES_H_ */
